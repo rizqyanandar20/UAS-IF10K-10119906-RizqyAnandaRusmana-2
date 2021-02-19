5d92be6039756b77e1d66e5c2e18d4b82ad175c9
rizqyanandar20 <rizqyananda.rusmana@gmail.com>
2021-02-19 20:54:20 +0700

    first commit

---
+++ src/uas/if10k/pkg10119906/rizqyanandarusmana/no2/UASIF10K10119906RizqyAnandaRusmanaNo2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.if10k.pkg10119906.rizqyanandarusmana.no2;
+
+/**
+ *
+ * @author senenngahenen
+ * nama         : Rizqy Ananda Rusmana
+   nim		: 10119906
+   kelas	: IF-10K
+   tanggal	: 19-Feb 2020
+   no. soal	: 5
+ */
+public class UASIF10K10119906RizqyAnandaRusmanaNo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        mahasiswa mhs1 = new mahasiswa();
+        mhs1.perkenalanDiri("10110269", "Rizki Adam Kurniawan");
+    }
+    
+}
---
+++ src/uas/if10k/pkg10119906/rizqyanandarusmana/no2/mahasiswa.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.if10k.pkg10119906.rizqyanandarusmana.no2;
+
+/**
+ *
+ * @author senenngahenen
+ * nama         : Rizqy Ananda Rusmana
+   nim		: 10119906
+   kelas	: IF-10K
+   tanggal	: 19-Feb 2020
+   no. soal	: 5
+ */
+public class mahasiswa {
+    String mahasiswa;
+    String nim;
+    
+    public void perkenalanDiri(String parNim, String parNama) {
+        System.out.println("Nim Mahasiswa : " +parNim);
+        System.out.println("Nama Mahasiswa : " +parNama);
+        System.out.println("");
+    }
+}
